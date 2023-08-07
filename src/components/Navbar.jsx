aa04e08964a83b3be419d699d0dd238d3d649b5a
Natalia <nany.nagisa@gmail.com>
2023-08-07 18:27:22 -0300

    style home query full

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Anchor from './Anchors'
 
 
+
+
 function Navbar() {
   const data = [
     { url: "#", content: "Home" },
@@ -13,6 +15,7 @@ function Navbar() {
       <Anchor />
       {data.map((element, index) => <Anchor key={index} url={element.url} content={element.content} />)}
       <button>Login</button>
+    
     </nav>
   )
 }
