d199df4075c2ca9925b5cd73f53cb819c43aecb5
Natalia <nany.nagisa@gmail.com>
2023-09-02 14:25:23 -0300

    redux in pregress3

--- src/pages/CityDetails.jsx
+++ src/pages/CityDetails.jsx
@@ -1,19 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Itineraries from '../components/Itineraries';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import axios from 'axios';
+import citiesActions from '../stores/actions/citiesAction';
 
 export default function CityDetails({ data }) {
     const { id } = useParams();
 
-    const [city, setCity] = useState({})
+    const [city, setCities] = useState({})
     useEffect(() => {
-        setCity(data.find(item => item._id === id))
+        setCities(data.find(item => item._id === id))
     }, [])
 
-let citiesInStore = useSelector(store => store.citiesInStore);
+    let citiesInStore = useSelector(store => store.citiesReducer.cities);
     console.log(citiesInStore);
-    
+
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        axios.get("http://localhost:3000/api/cities/")
+            .then((response) => {
+                setCities(response.data)
+                dispatch(citiesActions.add_cities(response.data))
+            })
+    }, []);
 
 
     const pageStyle = {
@@ -29,14 +40,18 @@ let citiesInStore = useSelector(store => store.citiesInStore);
 
     return (
         <>
-            <div className='cities-details' style={pageStyle}>
-                <div className='details-content'>
-                    <h2 className='details-title'>{city.place} </h2>
-                    <h4 className='details-subtitle'>{city.country}</h4>
-                    <h5 className='details-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae quas nihil laborum nisi laudantium? Assumenda doloribus, impedit ad debitis doloremque possimus reiciendis, eos numquam, nostrum voluptas blanditiis tempora esse minima.
-                        <a className='details-btn' class="nav-link" href="/cities">Back to Cities</a></h5>
-                </div>
+  <div className='cities-details' style={pageStyle}>
+        {citiesInStore.map((city, index) => (
+            <div className='details-content' key={index}>
+                <h2 className='details-title'>{city.place}</h2>
+                <h4 className='details-subtitle'>{city.country}</h4>
+                <h5 className='details-text'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae quas nihil laborum nisi laudantium? Assumenda doloribus, impedit ad debitis doloremque possimus reiciendis, eos numquam, nostrum voluptas blanditiis tempora esse minima.
+                    <a className='details-btn nav-link' href="/cities">Back to Cities</a>
+                </h5>
             </div>
-            <div><Itineraries /></div>
+        ))}
+    </div>
+    <div><Itineraries /></div>
         </>);
 }
