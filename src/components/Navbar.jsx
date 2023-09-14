6f3e0cf65cbe5c3ee1e27334d3bbd499db99e169
Natalia <nany.nagisa@gmail.com>
2023-09-14 15:52:46 -0300

    sprint4

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,13 @@
-import React, {useState, useEffect} from 'react';
-import { Navbar, Nav, Button } from 'react-bootstrap';
-import { BsFillPersonFill } from 'react-icons/bs';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { Navbar, Nav, Button } from "react-bootstrap";
+import { BsFillPersonFill } from "react-icons/bs";
+import { Link } from "react-router-dom";
 
-const NavigationBar = ({login}) => {
-  
-
+const NavigationBar = ({ login }) => {
   const handleLogOut = () => {
-    localStorage.removeItem("token")
+    localStorage.removeItem("token");
     window.location.reload();
-  }
+  };
 
   return (
     <Navbar bg="light" expand="lg">
@@ -17,23 +15,26 @@ const NavigationBar = ({login}) => {
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
           <Nav.Link href="/">Home</Nav.Link>
-          <Link to="/cities" className="nav-link">Cities</Link>
-          {
-            !login && (
-          <Button className="log-btn" variant="info">
-            <Link to="/register" className="nav-link">
-              <BsFillPersonFill className="mr-1 bg-transparent" /> Log In <i className="bi bi-person-fill"></i>
-            </Link>
-          </Button>
-            )
-          }
-           {
-            login && (
-          <Button className="log-btn danger" variant="info" onClick={()=>handleLogOut()}>
-              <BsFillPersonFill className="mr-1 bg-transparent" /> Log Out <i className="bi bi-person-fill"></i>
-          </Button>
-            )
-          }
+          <Link to="/cities" className="nav-link">
+            Cities
+          </Link>
+          {!login && (
+            <Button className="log-btn" variant="info">
+              <Link to="/register" className="nav-link">
+                <BsFillPersonFill className="mr-1 bg-transparent" /> Log In{" "}
+                <i className="bi bi-person-fill"></i>
+              </Link>
+            </Button>
+          )}
+          {login && (
+            <Button
+              className="log-btn btn-danger"
+              variant="info"
+              onClick={() => handleLogOut()}
+            >
+              Log Out
+            </Button>
+          )}
         </Nav>
       </Navbar.Collapse>
     </Navbar>
