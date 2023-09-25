5cd075f01592d3fffcbc344911c9461295de501c
Natalia <nany.nagisa@gmail.com>
2023-09-25 11:20:01 -0300

    Style revition

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,11 @@ function Header({login}) {
 
   return (
     <header>
-      <h2>My Tinerary</h2>
+      <div className='logo'>
+        <img className='icon' src="https://i.imgur.com/8d2dSKS.png" alt="" />
+      <img src="https://i.imgur.com/HsgGSYP.png" alt="title" srcset="" />
+      </div>
+      
       <Navbar login={login}/>
     </header>
   )
