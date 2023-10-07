17dda3f34acce1eb1e884efdb8ab4a9074f8e1e5
Natalia <nany.nagisa@gmail.com>
2023-10-06 21:10:04 -0300

    Style City

--- src/pages/CityDetails.jsx
+++ src/pages/CityDetails.jsx
@@ -1,110 +1,125 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import axios from 'axios';
-import citiesActions from '../stores/actions/citiesAction';
-import itinerariesActions from '../stores/actions/itinerariesActions';
-import PriceIcon from '../components/PriceIcon';
-import Accordion from 'react-bootstrap/Accordion';
-import { BsTools } from 'react-icons/bs';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import axios from "axios";
+import citiesActions from "../stores/actions/citiesAction";
+import itinerariesActions from "../stores/actions/itinerariesActions";
+import PriceIcon from "../components/PriceIcon";
+import Accordion from "react-bootstrap/Accordion";
+import { BsTools } from "react-icons/bs";
 
 export default function CityDetails({ data }) {
-    const { id } = useParams();
-    const dispatch = useDispatch();
+  const { id } = useParams();
+  const dispatch = useDispatch();
 
-    const [city, setCity] = useState(null);
-    const [itineraries, setItineraries] = useState([]);
+  const [city, setCity] = useState(null);
+  const [itineraries, setItineraries] = useState([]);
 
-    useEffect(() => {
-        const selectedCity = data.find(item => item._id === id);
-        setCity(selectedCity);
+  useEffect(() => {
+    const selectedCity = data.find((item) => item._id === id);
+    setCity(selectedCity);
 
-        axios.get(`http://localhost:3000/api/itineraries/${id}`)
-            .then((response) => {
-                console.log(response);
-                dispatch(citiesActions.add_cities(response.data));
-                setItineraries(response.data);
-            })
-            .catch((error) => {
-                console.error("Error to get API data:", error);
-            });
+    axios
+      .get(`http://localhost:3000/api/itineraries/${id}`)
+      .then((response) => {
+        console.log(response);
+        dispatch(citiesActions.add_cities(response.data));
+        setItineraries(response.data);
+      })
+      .catch((error) => {
+        console.error("Error to get API data:", error);
+      });
 
-        const itinerariesData = [];
-        dispatch(itinerariesActions.add_itineraries(itinerariesData));
-    }, [data, id, dispatch]);
+    const itinerariesData = [];
+    dispatch(itinerariesActions.add_itineraries(itinerariesData));
+  }, [data, id, dispatch]);
 
-    const pageStyle = {
-        backgroundImage: city ? `url(${city.image})` : '',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat',
-        minHeight: '100vh',
-    };
+  const pageStyle = {
+    backgroundImage: city ? `url(${city.image})` : "",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    minHeight: "100vh",
+  };
 
-
-
-    return (
-        <>
-            <div className='cities-details' style={pageStyle}>
-                <div className='details-content'>
-                    {city && (
-                        <>
-                            <h2 className='details-title'>{city.place}</h2>
-                            <h4 className='details-subtitle'>{city.country}</h4>
-                            <h5 className='details-text'>{city.info}
-                                <a className='call-btn details-btn ' href="/cities">Back to Cities</a>
-                            </h5>
-                        </>
-                    )}
+  return (
+    <>
+      <div className="cities-details" style={pageStyle}>
+        <div className="details-content d-flex">
+          {city && (
+            <>
+              <h2 className="details-title">{city.place}</h2>
+              <h4 className="details-subtitle">{city.country}</h4>
+              <h5 className="details-text d-flex">
+                {city.info}
+                <div className="">
+                  <a className="call-btn details-btn " href="/cities">
+                    Back to Cities
+                  </a>
                 </div>
+              </h5>
+            </>
+          )}
+        </div>
+      </div>
+
+      <div className="itineraries-container">
+        <h2>Itineraries</h2>
+
+        {itineraries.map((itinerary) => (
+          <div className="Itinerary" key={itinerary._id}>
+            <p className="itinerary-name">{itinerary.name}</p>
+            <div className="itinerary-block">
+              <p>Likes: {itinerary.likes}</p>
+
+              <div className="itinerary-info">
+                <div className="itinerary-user">
+                  <p>
+                    <div className="div-title">User:</div>{" "}
+                    {itinerary.authorName}
+                  </p>
+                  <img src={itinerary.authorPhoto} alt="authorPhoto" />
+                </div>
+                <div className="itinerary-hashtag">
+                  <p>
+                    <div className="div-title">Hashtags: </div>
+                    {itinerary.hashtags.join(", ")}
+                  </p>
+                </div>
+                <div className="itinerary-duration">
+                  <p>
+                    <div className="div-title">Duration:</div>{" "}
+                    {itinerary.duration} hours
+                  </p>
+                </div>
+                <div className="itinerary-price">
+                  <p>
+                    <div className="div-title">Price:</div>{" "}
+                    <PriceIcon price={itinerary.price} />
+                  </p>
+                </div>
+              </div>
+              <div className="itinerary-button">
+                <Accordion>
+                  <Accordion.Item eventKey="0">
+                    <Accordion.Header>View More</Accordion.Header>
+                    <Accordion.Body>
+                      <div className="accordeon-content">
+                        {" "}
+                        <BsTools />
+                        <p>UNDER CONSTRUCTION</p>
+                      </div>
+                    </Accordion.Body>
+                  </Accordion.Item>
+                </Accordion>
+              </div>
             </div>
-
-            <div className="itineraries-container">
-                <h2>Itineraries</h2>
-
-                {itineraries.map((itinerary) => (
-                    <div className='Itinerary' key={itinerary._id}>
-                        <p className='itinerary-name'>{itinerary.name}</p>
-                        <div className='itinerary-block'>
-                            <p>Likes: {itinerary.likes}</p>
-
-                            <div className='itinerary-info'>
-                                <div className='itinerary-user'>
-                                    <p><div className='div-title'>User:</div> {itinerary.authorName}</p>
-                                    <img src={itinerary.authorPhoto} alt="authorPhoto" />
-                                </div>
-                                <div className='itinerary-hashtag'>
-                                    <p><div className='div-title'>Hashtags: </div>
-                                        {itinerary.hashtags.join(', ')}</p>
-                                </div>
-                                <div className='itinerary-duration'>
-                                    <p><div className='div-title'>Duration:</div> {itinerary.duration} hours</p>
-                                </div>
-                                <div className='itinerary-price'>
-                                    <p><div className='div-title'>Price:</div> <PriceIcon price={itinerary.price} /></p>
-
-                                </div>
-                            </div>
-                            <div className='itinerary-button'>
-                                <Accordion>
-                                    <Accordion.Item eventKey="0">
-                                        <Accordion.Header>View More</Accordion.Header>
-                                        <Accordion.Body>
-                                            <div className='accordeon-content'>  <BsTools />
-                                        <p>UNDER CONSTRUCTION</p></div>
-                                      
-                                        </Accordion.Body>
-                                    </Accordion.Item>
-                                </Accordion>
-                            </div>
-                        </div>
-                    </div>
-                ))}
-
-            </div>
-        </>
-    );
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
